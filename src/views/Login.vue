dd4fa866cb5b5665fa2ed6ea3c9c66c0b365257d
Gaurav-dawadi <gaurabdawadi@gamil.com>
2021-02-23 23:28:47 +0545

    fix: minor issues

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -5,21 +5,12 @@
         <div class="col-md-6 offset-md-3 col-xs-12">
           <h1 class="text-xs-center">Sign up</h1>
           <p class="text-xs-center">
-            <a href="">Have an account?</a>
+            <router-link to="/register">
+              Need an account?
+            </router-link>
           </p>
 
-          <ul class="error-messages">
-            <li>That email is already taken</li>
-          </ul>
-
           <form>
-            <fieldset class="form-group">
-              <input
-                class="form-control form-control-lg"
-                type="text"
-                placeholder="Your Name"
-              />
-            </fieldset>
             <fieldset class="form-group">
               <input
                 class="form-control form-control-lg"
@@ -35,7 +26,7 @@
               />
             </fieldset>
             <button class="btn btn-lg btn-primary pull-xs-right">
-              Sign up
+              Sign in
             </button>
           </form>
         </div>
--- src/views/Register.vue
+++ src/views/Register.vue
@@ -5,12 +5,14 @@
         <div class="col-md-6 offset-md-3 col-xs-12">
           <h1 class="text-xs-center">Sign up</h1>
           <p class="text-xs-center">
-            <a href="">Have an account?</a>
+            <router-link to="/login">
+              Have an account?
+            </router-link>
           </p>
 
-          <ul class="error-messages">
+          <!-- <ul class="error-messages">
             <li>That email is already taken</li>
-          </ul>
+          </ul> -->
 
           <form>
             <fieldset class="form-group">
