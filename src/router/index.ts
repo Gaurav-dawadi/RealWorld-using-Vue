60040ca8662db8b31c962053887012e0715081ad
Gaurav-dawadi <gaurabdawadi@gamil.com>
2021-02-23 10:14:17 +0545

    feat: add routing for pages

---
+++ src/router/index.ts
@@ -0,0 +1,34 @@
+import Vue from 'vue'
+import VueRouter, { RouteConfig } from 'vue-router'
+import Home from '../views/Home.vue'
+
+Vue.use(VueRouter)  
+
+const routes: Array<RouteConfig> = [
+  {
+    path: '/',
+    name: 'Home',
+    component: Home
+  },
+  {
+    path: '/login',
+    name: 'login',
+    component: () => import('../views/Login.vue'),
+  },
+  {
+    path: '/register',
+    name: 'register',
+    component: () => import('../views/Register.vue'),
+  },
+  {
+    path: '/settings',
+    name: 'settings',
+    component: () => import('../views/Settings.vue'),
+  }
+]
+
+const router = new VueRouter({
+  routes
+})
+
+export default router
